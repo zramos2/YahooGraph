22718aa785e4db0dc2a1ad5ffd89a3784b48cd54
ZachRamos <zachariramos@gmail.com>
2020-01-23 11:35:18 -0600

    added dev.tar, dev, and staging

---
+++ dev/trade3.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# This version reads in an input as a stock symbol
+# August 24, 2019
+
+import time
+import requests
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+# import numpy as np
+
+# url = 'https://finance.yahoo.com/quote/FAS/options?date=1564704000'
+begin_url = 'https://finance.yahoo.com/quote/'
+end_url = '/options/'
+
+stock_symbol = ""
+while(stock_symbol != 'ZZZ'):
+    #stock_symbol = input("Enter a stock symbol: ")
+    print('Generating graph')
+    time.sleep(3)
+    stock_symbol = 'FAS'
+    if stock_symbol == 'ZZZ':
+        break
+    print(stock_symbol)
+    url = begin_url + stock_symbol + end_url
+    print(url)
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    rows = soup.select('.calls tbody tr')
+
+    strike = []     # Y-axis
+    interest = []   # X-axis
+    for row in rows:
+
+        strike_price = row.select_one('.data-col2').text.strip()
+        strike_price = float(strike_price)
+        strike.append(strike_price)
+        open_interest = row.select_one('.data-col9').text.strip()
+        open_interest = int(open_interest.replace(',', ''))
+        interest.append(open_interest)
+    """
+    print(strike[0])
+    print(interest[0])
+    print(len(rows))
+    """
+
+    fig = plt.figure(figsize=(9, 6))
+    graph = fig.add_axes([0.1, 0.1, 0.9, 0.9])
+    graph.scatter(interest, strike)
+    graph.set_xlabel('Open Interest')
+    graph.set_ylabel('Strike Price')
+    plt.show()
+
+print("Input = 'ZZZ' Ending loop. ")
---
+++ staging/trade4INPUT.py
@@ -0,0 +1,75 @@
+# This version reads a text file filled with stock symbols
+
+import requests
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+# import numpy as np
+
+"""
+def find_index(array):
+    index_val = []
+    for i in range(len(array)):
+        x = array[i] - array[i-1]
+"""
+
+
+# url = 'https://finance.yahoo.com/quote/FAS/options?date=1564704000'
+begin_url = 'https://finance.yahoo.com/quote/'
+end_url = '/options/'
+textfile = open("trade4INPUT.txt", "r")
+# print(textfile.read())
+# print(textfile.readlines())
+
+
+# total_index = []  # will be subtracted with 'indexVal' to get correct index value
+actualIndx = []  # same values as 'get_index' its just outside of for loop
+strike = []     # Y-axis
+interest = []   # X-axis
+get_title = []
+numSym = 0
+for line in textfile:
+    numSym += 1
+
+    url = begin_url + line.rstrip() + end_url
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    rows = soup.select('.calls tbody tr')
+
+    get_index = 0
+
+    for row in rows:
+        strike_price = row.select_one('.data-col2').text.strip()
+        strike_price = float(strike_price)
+        strike.append(strike_price)
+        open_interest = row.select_one('.data-col9').text.strip()
+        if open_interest.find(',') != -1:
+            open_interest = int(open_interest.replace(',', ''))
+        elif open_interest.find('-') != -1:
+            open_interst = 0
+        else:
+            #print('open_interest: ', open_interest)
+            open_interest = int(open_interest)
+        interest.append(open_interest)
+        get_index += 1
+
+    actualIndx.append(get_index)
+    get_title.append(line.rstrip())
+#    total_index.append(len(strike))
+#    print(len(strike), ' = len(strike)')
+#    print(total_index, ' = total_index')
+#    print(actualIndx, ' = actualIndx')
+nrows = 3
+ncols = 2
+k = 0
+fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(8, 6))
+
+for j in range(nrows):
+    for i in range(ncols):
+        axs[j, i].scatter(interest[:actualIndx[k]], strike[:actualIndx[k]])
+        axs[j, i].set_title(get_title[k])
+        axs[j, i].set_xlabel('Open Interest')
+        axs[j, i].set_ylabel('Strike Price')
+        k += 1
+
+plt.tight_layout()
+plt.show()
